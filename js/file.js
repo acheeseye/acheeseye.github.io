785b671748b0c079c9fbf6f78b8bee1a062090c0
Acheeseye <hsi_jason@hotmail.com>
2019-08-27 14:58:52 -0800

    increased offset var accessibility, changed scroll velocity, sticky function, added header

--- js/file.js
+++ js/file.js
@@ -2,17 +2,19 @@ $(document).ready(function () {
     $("[data-menu-underline-from-center] a").addClass("underline-from-center");
 });
 
+var scrollToExtraTopOffset = 60;
+
 var section1 = $("#section1"),
-    section1Top = section1.offset().top - 45,
+    section1Top = section1.offset().top - scrollToExtraTopOffset,
 
     section2 = $("#section2"),
-    section2Top = section2.offset().top - 45,
+    section2Top = section2.offset().top - scrollToExtraTopOffset,
 
     section3 = $("#section3"),
-    section3Top = section3.offset().top - 45,
+    section3Top = section3.offset().top - scrollToExtraTopOffset,
 
     section4 = $("#section4"),
-    section4Top = section4.offset().top - 45,
+    section4Top = section4.offset().top - scrollToExtraTopOffset,
 
     section1Btn = document.getElementById("section1Btn"),
     section2Btn = document.getElementById("section2Btn"),
@@ -20,14 +22,27 @@ var section1 = $("#section1"),
     section4Btn = document.getElementById("section4Btn");
 
 section1Btn.onclick = function () {
-    TweenMax.to(window, 1, { scrollTo: section1Top, ease: Power1.easeOut });
+    TweenMax.to(window, 1.5, { scrollTo: section1Top, ease: Expo.easeOut });
 }
 section2Btn.onclick = function () {
-    TweenMax.to(window, 1, { scrollTo: section2Top, ease: Power1.easeOut });
+    TweenMax.to(window, 1.5, { scrollTo: section2Top, ease: Expo.easeOut });
 }
 section3Btn.onclick = function () {
-    TweenMax.to(window, 1, { scrollTo: section3Top, ease: Power1.easeOut });
+    TweenMax.to(window, 1.5, { scrollTo: section3Top, ease: Expo.easeOut });
 }
 section4Btn.onclick = function () {
-    TweenMax.to(window, 1, { scrollTo: section4Top, ease: Power1.easeOut });
+    TweenMax.to(window, 1.5, { scrollTo: section4Top, ease: Expo.easeOut });
+}
+
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
 }
