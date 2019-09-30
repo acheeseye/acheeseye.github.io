9ee50c7ecb3e052117d9122ef2deacdca59f25c8
Acheeseye <hsi_jason@hotmail.com>
2019-09-29 16:53:50 -0800

    added logic for determining which section is active

--- js/file.js
+++ js/file.js
@@ -22,9 +22,11 @@ var sectionTop = $("#sectionTop"),
     section2Btn = document.getElementById("section2Btn"),
     section3Btn = document.getElementById("section3Btn");
 
+
+
 section0Btn.onclick = function () {
     TweenMax.to(window, scrollToDuration, { scrollTo: section1Top, ease: Expo.easeOut });
-    // TweenMax.from(".menu", 0.5, { right: -100, ease: Power2.easeOut });
+    // TweenMax.from(".menu", 0.5, { right: -100, ease: Power2.ease });
 }
 sectionTopBtn.onclick = function () {
     TweenMax.to(window, scrollToDuration, { scrollTo: sectionTopTop, ease: Expo.easeOut });
@@ -47,6 +49,8 @@ var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function stickyFunction() {
+    // console.log(window.pageYOffset);
+    // console.log(sticky);
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
     } else {
@@ -130,7 +134,7 @@ function drawChart() {
 
         },
         chartArea: {
-            left: 10,
+            left: 0,
             width: '100%',
         },
     };
@@ -143,3 +147,56 @@ function drawChart() {
     AIChart.draw(AIData, options);
     calcChart.draw(calcData, options);
 }
+
+
+var menuContainer = document.getElementById("sidenav");
+var menuItems = menuContainer.getElementsByClassName("underline-from-center");
+var sectionTriggerOffset = vh_value / 2;
+var sectionTriggerPoints = [section1Top - sectionTriggerOffset, section2Top - sectionTriggerOffset, section3Top - sectionTriggerOffset];
+var lastTriggerPointIndex = sectionTriggerPoints.length - 1;
+// console.log(sectionTriggerPoints);
+
+$(window).scroll(function () {
+    var fromTop = $(this).scrollTop(); // window's scroll position
+    // console.log(fromTop);
+    // console.log("menuItems: " + menuItems.length);
+    for (var i = 0; i < menuItems.length; i++) {
+        // console.log(i + ": " + menuItems[i].offsetTop);
+        // console.log("\n");
+        // console.log(fromTop);
+        if (fromTop > sectionTriggerPoints[lastTriggerPointIndex - 1]) {
+            // console.log("past point 1");
+            if (fromTop > sectionTriggerPoints[lastTriggerPointIndex]) {
+                // console.log(lastTriggerPointIndex);
+                var previous = document.getElementsByClassName("activeTab");
+                previous[0].className = previous[0].className.replace(" activeTab", "");
+                menuItems[lastTriggerPointIndex].className += " activeTab";
+                return lastTriggerPointIndex;
+            }
+        }
+        if (sectionTriggerPoints[i] < fromTop && fromTop < sectionTriggerPoints[i + 1]) {
+            var previous = document.getElementsByClassName("activeTab");
+            previous[0].className = previous[0].className.replace(" activeTab", "");
+            menuItems[i].className += " activeTab";
+            return i;
+        }
+        // if (sectionTriggerPoints[lastTriggerPoint] < fromTop) {
+        //     console.log(lastTriggerPoint);
+        // }
+    }
+    // }
+    // var cur = menuItems.map(function () {
+    //     if ($(this).offset().top < fromTop) {
+    //         return this;
+    //     }
+    // });
+})
+
+// for (var i = 0; i < menuItems.length; i++) {
+//     menuItems[i].addEventListener("click", function () {
+//         var current = document.getElementsByClassName("activeTab");
+//         current[0].className = current[0].className.replace(" activeTab", "");
+//         this.className += " activeTab";
+//     }
+//     )
+// }
